c6434d9a6cdf5aa739fe855da9ea01580781a886
Ruth Wangare AideenHah-yil <wangarenjorogeruth@gmail.com>
2024-03-12 11:20:48 -0700

    initial commit

---
+++ score.py
@@ -0,0 +1,10 @@
+score = 90
+
+if score >= 80:
+    print("Distinction")
+elif 60 <= score <= 70:
+    print("Credit")
+elif 40 <= score <= 50:
+    print("Fair")
+elif score < 40:
+    print("Failed")
